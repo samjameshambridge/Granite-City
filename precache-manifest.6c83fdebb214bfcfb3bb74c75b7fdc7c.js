6c178843836ef39f3e2070884ecc02c82488decf
Sam Hambridge <samhambridge@hotmail.com>
2019-05-29 19:37:34 +0100

    Updates

---
+++ precache-manifest.6c83fdebb214bfcfb3bb74c75b7fdc7c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25b052b43a410ddd21bce4165aff284",
+    "url": "/Granite-City/index.html"
+  },
+  {
+    "revision": "8f128e6d99ec3ab66f26",
+    "url": "/Granite-City/static/css/main.033d679a.chunk.css"
+  },
+  {
+    "revision": "aa6244c986fd3d0b20b5",
+    "url": "/Granite-City/static/js/2.bd2159a1.chunk.js"
+  },
+  {
+    "revision": "8f128e6d99ec3ab66f26",
+    "url": "/Granite-City/static/js/main.c0cac222.chunk.js"
+  },
+  {
+    "revision": "e956d5fbfa6a5229e26c",
+    "url": "/Granite-City/static/js/runtime~main.f1260400.js"
+  },
+  {
+    "revision": "13dc531a2ffd9ca7eeb500515774fa74",
+    "url": "/Granite-City/static/media/GlacialIndifference-Regular.13dc531a.otf"
+  },
+  {
+    "revision": "9e273b203ce37047568f4905f9279169",
+    "url": "/Granite-City/static/media/GlacialIndifference-Regular.9e273b20.ttf"
+  },
+  {
+    "revision": "a6509aa3161205ebe01c0f7e2f209b44",
+    "url": "/Granite-City/static/media/LeagueSpartan-Bold.a6509aa3.otf"
+  },
+  {
+    "revision": "b620643d0b627e045ec4bcc7e53452fd",
+    "url": "/Granite-City/static/media/LeagueSpartan-Bold.b620643d.ttf"
+  },
+  {
+    "revision": "79d5bfd9887f559f8f57c5de6c5df674",
+    "url": "/Granite-City/static/media/cake.79d5bfd9.png"
+  },
+  {
+    "revision": "3efe30d67dfab21fd17dbacb97272582",
+    "url": "/Granite-City/static/media/chalk-bag.3efe30d6.png"
+  },
+  {
+    "revision": "1cf9a4a954d17fe50679b5635e3f7de5",
+    "url": "/Granite-City/static/media/coffee-cup.1cf9a4a9.png"
+  },
+  {
+    "revision": "17f5c76d4168e9c93f562d2d68e44b0c",
+    "url": "/Granite-City/static/media/dotted-border.17f5c76d.png"
+  },
+  {
+    "revision": "c0cd138271276bb33a87c2d174015455",
+    "url": "/Granite-City/static/media/facebook.c0cd1382.svg"
+  },
+  {
+    "revision": "b1c58ee19d1e2c38b29ae86f3174f2e4",
+    "url": "/Granite-City/static/media/instagram.b1c58ee1.svg"
+  },
+  {
+    "revision": "58550debccbea4c1c5d1f688674c536a",
+    "url": "/Granite-City/static/media/landing-page-pic.58550deb.png"
+  },
+  {
+    "revision": "4b475fb3f54b3e35daa04295ec48df4c",
+    "url": "/Granite-City/static/media/linkedin.4b475fb3.svg"
+  },
+  {
+    "revision": "5573febd30180097f27938f7bc3f52ae",
+    "url": "/Granite-City/static/media/logo-copy.5573febd.png"
+  },
+  {
+    "revision": "f39e568b1405fbf5b8d33f657e0c2ce4",
+    "url": "/Granite-City/static/media/shoe.f39e568b.png"
+  },
+  {
+    "revision": "c9f1200836515fd4bacd29160be78732",
+    "url": "/Granite-City/static/media/twitter.c9f12008.svg"
+  },
+  {
+    "revision": "37729d224b666b3e130fc273ebd98b0a",
+    "url": "/Granite-City/static/media/welcome-info-img.37729d22.jpg"
+  },
+  {
+    "revision": "8d60b63d7ce3ec9fa3e93769f3f8e61b",
+    "url": "/Granite-City/static/media/welcome-landing-img.8d60b63d.png"
+  }
+]);
